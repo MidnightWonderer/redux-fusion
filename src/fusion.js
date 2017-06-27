f963fd478e8319079e52724be2caac49d4af88c3
Sarun Rattanasiri <midnight_w@gmx.tw>
2017-06-28 00:08:37 +0700

    support multiple store

--- src/fusion.js
+++ src/fusion.js
@@ -1,3 +1,7 @@
+import { default as find } from 'lodash/fp/find'
+import { default as get } from 'lodash/fp/get'
+import { default as identity } from 'lodash/fp/identity'
+import { default as unionBy } from 'lodash/fp/unionBy'
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { componentFromStreamWithConfig } from 'recompose/componentFromStream'
@@ -6,44 +10,85 @@ import { default as shallowEqual } from 'recompose/shallowEqual'
 import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 import { ReplaySubject } from 'rxjs/ReplaySubject'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/let'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/publishBehavior'
 import 'rxjs/add/operator/publishReplay'
+import 'rxjs/add/operator/scan'
 
-const namespece = 'reduxFusion'
+const namespece = 'ReduxFusion/'
 const componentFromStream = componentFromStreamWithConfig(rxjsConfig)
 const stateProxy = new ReplaySubject(1)
-const nullifiedState = {}
-const stateSpy = (storeState) => {
-  stateProxy.next(storeState)
-  return nullifiedState
-}
+const stateKey = `${namespece}State`
 const dispatchKey = `${namespece}Dispatch`
+const stateSpy = (storeState) => ({ [stateKey]: storeState })
 const dispatchSpy = (dispatch) => ({ [dispatchKey]: dispatch })
 const connectSpy = connect(stateSpy, dispatchSpy)
-const state$ = stateProxy.distinctUntilChanged().publishReplay(1).refCount()
+const selectKey = get(['key'])
+const unwrapState = get(['state'])
+const allState$ = stateProxy
+  .scan((allState, updatingState) =>
+    unionBy(selectKey, [updatingState], allState),
+    [],
+  )
+  .publishBehavior([])
+  .refCount()
+
+const mutableStateCache = []
+const getStateForKey = (key) => {
+  const cached = find({ key }, mutableStateCache)
+  if (cached) {
+    return cached.state$
+  }
+  const selectedState$ = allState$
+    .map(find({ key }))
+    .filter(identity)
+    .map(unwrapState)
+    .distinctUntilChanged()
+    .publishReplay(1)
+    .refCount()
+  mutableStateCache.push({
+    key,
+    state$: selectedState$,
+  })
+}
 
 const fuse = (connectHandler) => (Component) => {
-  class FusedComponent extends React.Component {
+  const applyPropsStream = (applyingProps) => <Component {...applyingProps}/>
+  class AdapterComponent extends React.Component {
     constructor(props) {
       super(props)
-      const { [dispatchKey]: dispatch, ...pureProps } = props
+      const {
+        [dispatchKey]: dispatch,
+        [stateKey]: storeState,
+        ...pureProps,
+      } = props
+      // in order to make redux-fusion reusable
+      // across multiple store we assign dispatch as key
+      // and use it to distinguish between each store
+      stateProxy.next({ key: dispatch, state: storeState })
+      const state$ = getStateForKey(dispatch)
       const propsProxy = new BehaviorSubject(pureProps)
       const propsHandler = connectHandler(state$, dispatch)
       const transformedProps$ = propsProxy
         .distinctUntilChanged(shallowEqual)
         .let(propsHandler)
         .distinctUntilChanged(shallowEqual)
-      const applyPropsStream = (transformedProps) => <Component {...transformedProps}/>
       this.propsProxy = propsProxy
-      this.ConnectedComponent = componentFromStream(
+      this.InnerComponent = componentFromStream(
         () => transformedProps$.map(applyPropsStream),
       )
     }
 
     componentWillReceiveProps(nextProps) {
       // eslint-disable-next-line no-unused-vars
-      let { [dispatchKey]: unused, ...pureProps } = nextProps;
+      const {
+        [dispatchKey]: dispatch,
+        [stateKey]: storeState,
+        ...pureProps,
+      } = nextProps
+      stateProxy.next({ key: dispatch, state: storeState })
       this.propsProxy.next(pureProps)
     }
 
@@ -52,15 +97,15 @@ const fuse = (connectHandler) => (Component) => {
     }
 
     propsProxy
-    ConnectedComponent
+    InnerComponent
 
     render() {
-      const { ConnectedComponent } = this
-      return <ConnectedComponent/>
+      const { InnerComponent } = this
+      return <InnerComponent/>
     }
   }
 
-  return connectSpy(FusedComponent)
+  return connectSpy(AdapterComponent)
 }
 
 export default fuse
